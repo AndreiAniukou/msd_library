c71d7d44403af8a709a6b9ed8390d5d3aea19ac1
Andrei Aniukou <andrei.aniukou@gcore.lu>
2020-01-20 11:53:07 +0300

    Update models.py and modify admin.py

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 from catalog.models import Author, Genre, Book, BookInstance
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    #fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 
-admin.site.register(Book)
-admin.site.register(Author)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'display_genre']
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ['status', 'due_back']
+
+
+#admin.site.register(Book)
+admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+#admin.site.register(BookInstance)
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -19,6 +19,7 @@ class Language(models.Model):
     def __str__(self):
         return self.language
 
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
@@ -27,6 +28,11 @@ class Book(models.Model):
                                                           'book')
     isbn = models.CharField('IBSN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+
+
+    def display_genre(self):
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+    display_genre.short_description = 'Genre'
 
 
     def __str__(self):
